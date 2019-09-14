6c18630df77f34a4d1cb684e7004cef1e773454f
chickenppox <borys6ff@gmail.com>
2019-09-14 14:48:43 +0200

    completed version

--- app/src/main/java/com/example/recycleviewapplication/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewapplication/MainActivity.kt
@@ -1,21 +1,106 @@
 package com.example.recycleviewapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentResolver
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val contactsList = arrayListOf<String>()
+        val numberList = arrayListOf<String>()
+        val idList = arrayListOf<String>()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //czemu potrzebne ?
+        if(savedInstanceState != null)
+            screen.text = savedInstanceState.getString("Number")
+        else
+            screen.text = "${Random().nextInt(100)}"
+
+        Log.d("ZMIANA", "Wykonala sie metoda onCreate")
+
+        //Element odpowiedzialny za ustawienie widokow w formie listy
         var recyclerView = findViewById<RecyclerView>(R.id.RecyclerView)
-        //tworzy liste
+        //Element odpowiedzielny za polaczenie miedzy danymi, a widokami
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         recyclerView.adapter = MyAdapter()
+
+        val contentResolver = contentResolver
+        var cursor = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null, null, null, null, null
+        )
+
+        try {
+
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    var id =
+                        cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID))
+                    idList.add(id)
+                    val name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY))
+                    contactsList.add(name)
+
+                    readPhoneNumber(contentResolver, id)
+                }
+
+
+            }
+
+        } finally {
+            cursor?.close()
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("Number",screen.text.toString())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        idList.clear()
+        numberList.clear()
+        contactsList.clear()
+
+        Log.d("ZMIANA", "Wykonala sie metoda onDestroy")
+
+    }
+
+    fun readPhoneNumber(contentResolver: ContentResolver, id: String) {
+
+        val phoneCursor = contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "= ?", arrayOf(id), null
+        )
+
+        if (phoneCursor != null) {
+            if (phoneCursor.moveToFirst()) {
+                var number = phoneCursor.getString(
+                    phoneCursor
+                        .getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                )
+                numberList.add(number)
+            } else
+                numberList.add("No number")
+        }
+
+        phoneCursor?.close()
+
+
     }
 }
--- app/src/main/java/com/example/recycleviewapplication/MyAdapter.kt
+++ app/src/main/java/com/example/recycleviewapplication/MyAdapter.kt
@@ -1,9 +1,18 @@
 package com.example.recycleviewapplication
 
+import android.content.ContentUris
+import android.content.Intent
+import android.net.Uri
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleviewapplication.MainActivity.Companion.contactsList
+import com.example.recycleviewapplication.MainActivity.Companion.idList
+import com.example.recycleviewapplication.MainActivity.Companion.numberList
 import kotlinx.android.synthetic.main.contact_row.view.*
 
 class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
@@ -15,15 +24,64 @@ class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return ContactDataBase.contactList.size
+        return contactsList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val name = holder.view.contact_name
         val number = holder.view.contact_number
+        val callBT = holder.view.call_BT
+        val smsBT = holder.view.sms_BT
+        //val contentResolver = holder.itemView.context.contentResolver
 
-        name.text = ContactDataBase.contactList[position]
-        number.text = ContactDataBase.contactNumberList[position]
+        holder.itemView.setOnLongClickListener(object: View.OnLongClickListener{
+            override fun onLongClick(v: View?): Boolean {
+                val contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI,
+                    idList[holder.adapterPosition].toLong())
+
+                val editIntent = Intent()
+                    editIntent.action = Intent.ACTION_EDIT
+                    editIntent.data = contactUri
+
+                startActivity(holder.itemView.context, editIntent, null)
+
+                return false
+            }
+        })
+
+
+        name.text = contactsList[position]
+        number.text = numberList[position]
+
+        smsBT.setOnClickListener{
+            val smsIntent = Intent()
+            if(numberList[position] == "No number") {
+                Toast.makeText(holder.itemView.context, "Invalid number or no number", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            else {
+                smsIntent.data = Uri.parse("sms: " + numberList[position])
+                smsIntent.action = Intent.ACTION_VIEW
+                startActivity(holder.itemView.context, smsIntent, null)
+            }
+        }
+
+        callBT.setOnClickListener {
+            val callIntent = Intent()
+            if(numberList[position] == "No number") {
+                Toast.makeText(holder.itemView.context, "Invalid number or no number", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            else {
+                callIntent.data = Uri.parse("tel: " + numberList[position])
+                callIntent.action = Intent.ACTION_CALL
+                startActivity(holder.itemView.context, callIntent, null)
+            }
+        }
+
+
     }
 
 }
